a45d24a174c610a3706f31af345b2957fc3e912a
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-21 22:00:09 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(veggies)
 end
 
 def long_planeteer_calls(calls_long)
-  calls_long.any? { |call| call > 4 }
+  calls_long.any? { |call| call.length > 4 }
 end
 
 def find_the_cheese# code an argument here
